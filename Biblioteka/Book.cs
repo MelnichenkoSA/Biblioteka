2d523a71ab98748f412cb525d386cbb8d2f76b2d
melnichenkosa <melnichenkosa@402D-6>
2023-09-20 12:10:33 +0700

    Добавьте файлы проекта.

---
+++ Biblioteka/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    internal class Book
+    {
+        public string Author;
+        public short Acr;
+        public List<DateTime> Data;
+        public int Count;
+
+        public Book(string Author, short Acr, int Count) 
+        {
+            this.Author = Author;
+            this.Acr = Acr;
+            this.Count = Count;
+        }
+    }
+}
---
+++ Biblioteka/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    internal class User
+    {
+        public int Id = 0;
+        public string Name;
+        public string Family;
+        public List<Book> Books;
+
+        public User(string Name, string Family)
+        {
+            this.Name = Name;
+            this.Family = Family;
+        }
+
+    }
+}
