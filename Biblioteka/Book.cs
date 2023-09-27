e6c983404ef264b46ecfa0413ec02d0cb4a9d762
MelnichenkoSA <112738977+MelnichenkoSA@users.noreply.github.com>
2023-09-27 15:53:10 +0700

    Update Book.cs

--- Biblioteka/Book.cs
+++ Biblioteka/Book.cs
@@ -11,14 +11,23 @@ namespace Biblioteka
     {
         public string Author;
         public short Acr;
-        public List<DateTime> Data;
+        public string[,] Data;
         public int Count;
+        public int Counter;
 
         public Book(string Author, short Acr, int Count) 
         {
             this.Author = Author;
             this.Acr = Acr;
             this.Count = Count;
+            Data = new string[1,Count];
+            Counter = 0;
+        }
+
+        public void Die(User user)
+        {
+            Data[1, Counter] = Convert.ToString(user.Id);
+            Counter += 1;
         }
     }
 }
