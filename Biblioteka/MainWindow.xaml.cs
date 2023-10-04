9f81a36fca9e1990d14bf8c4bfebf804da5ce5f4
MelnichenkoSA <112738977+MelnichenkoSA@users.noreply.github.com>
2023-10-04 10:41:34 +0700

    Update MainWindow.xaml.cs

--- Biblioteka/MainWindow.xaml.cs
+++ Biblioteka/MainWindow.xaml.cs
@@ -23,6 +23,40 @@ namespace Biblioteka
         public MainWindow()
         {
             InitializeComponent();
+
+            User newUser = new User(1, "Имя", "Фамилия");
+            libraryManager.AddUser(newUser);
+
+            Book newBook = new Book("Название книги", "Автор книги", new DateTime(2023, 1, 1), 5);
+            libraryManager.AddBook(newBook);
+        }
+
+        private LibraryManager libraryManager = new LibraryManager();
+
+        private void FindUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            int userId = int.Parse(userIdTextBox.Text);
+            User foundUser = libraryManager.FindUser(userId);
+        }
+
+        private void IssueBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            User selectedUser = userki.SelectedItem as User;
+            Book selectedBook = Booki.SelectedItem as Book;
+            if (selectedUser != null && selectedBook != null)
+            {
+                libraryManager.IssueBook(selectedUser, selectedBook);
+            }
+        }
+
+        private void ReturnBookButton_Click(object sender, RoutedEventArgs e)
+        {
+            User selectedUser = userki.SelectedItem as User;
+            Book returnedBook = Booki.SelectedItem as Book;
+            if (selectedUser != null && returnedBook != null)
+            {
+                libraryManager.ReturnBook(selectedUser, returnedBook);
+            }
         }
     }
 }
